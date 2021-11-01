455d7facaf1238f1b14264d696dbb723ac6b99a5
Victorvargas29 <vic7orvargas@gmail.com>
2021-11-01 09:17:02 -0400

    placa al reporte de presupuesto

--- report/presupuestoPdf.php
+++ report/presupuestoPdf.php
@@ -145,6 +145,13 @@ label{
             </div>
             
     </div>
+    <div > 
+            <div class="" style="display: inline-block">
+              <label class="Estilo3" style="margin-top: 50%">PLACA:</label>
+              <label id=""><?php echo $venta[0]["placa"];?></label>
+            </div>
+                        
+    </div>
    
 <!-- 
 <div>  
